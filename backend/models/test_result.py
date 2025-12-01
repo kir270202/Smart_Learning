041a6dafaac96a6d926a98650165dea52ee38ce3
kir270202 <kir.27.artemov@gmail.com>
2025-12-01 11:50:07 +0100

    Created TestResult table

---
+++ backend/models/test_result.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+from typing import Optional
+from sqlmodel import SQLModel, Field
+
+
+class TestResultBase(SQLModel):
+    user_id: int = Field(foreign_key="user.id")
+    subject_test_id: int = Field(foreign_key="subjecttest.id")
+    correct_answered: int
+    total_questions: int
+
+
+class TestResult(TestResultBase, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+
+    timestamp: datetime = Field(default_factory=datetime.utcnow, nullable=False)
