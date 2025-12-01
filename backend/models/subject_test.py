0c668e5986c4f45574c1cae897f624e28b29de42
kir270202 <kir.27.artemov@gmail.com>
2025-12-01 11:36:09 +0100

    Created table for SubjectTests

---
+++ backend/models/subject_test.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+from typing import Optional
+from sqlmodel import SQLModel, Field
+
+
+class SubjectTestBase(SQLModel):
+    subject_id: int = Field(foreign_key="subject.id")
+    test: Optional[str] = None  # stored as text
+    question_type: Optional[str] = None
+
+
+class SubjectTest(SubjectTestBase, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+
+    created_at: datetime = Field(default_factory=datetime.utcnow, nullable=False)
+    updated_at: datetime = Field(default_factory=datetime.utcnow, nullable=False)
